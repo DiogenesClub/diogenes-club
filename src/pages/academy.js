7e6f22ee12c41721f516996d02831fa546cfef96
Noelia Martínez <noeowo384@gmail.com>
2022-03-03 21:55:13 -0400

    added card on academy page

--- src/pages/academy.js
+++ src/pages/academy.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useRef,useEffect} from 'react';
 import gsap from 'gsap';
 import { Title } from '../components/title';
 import Transition from '../components/transition';
+import { ContentCard } from './academy.style';
+import Card from '../components/card';
 
 const Academy = () => {
     const transitionline = gsap.timeline();
+    let card = useRef(null);
+    useEffect(() => {
+      gsap.from(card,1,{
+          opacity: 0,
+          y:-40,
+          x:40,
+          ease: "power3.out",
+          stagger: {
+            amount: 0.15
+        }
+      })
+    })
     return(
         <div>
             <Transition timetransition={transitionline}/>
             <Title lineContent="This is" lineContent2="Academy"/>
+            <ContentCard ref={el => card = el}>
+                <Card/>
+            </ContentCard>
         </div>
     )
 }
---
+++ src/pages/academy.style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ContentCard = styled.div`
+    width: 100%;
+    margin-top: 10px;
+    padding: 10px;
+    overflow: hidden;
+    text-align: center;
+    background-color: white;
+    height: auto;
+`;
